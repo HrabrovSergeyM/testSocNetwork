6de49024ea5e331bc5a8db029e34dbc90cbff40d
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-02-10 18:42:53 +0300

    Updates

---
+++ precache-manifest.b01e9483bfef8bef168962bcfeaf43a3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5e3868bd5b62d84937958ebe31a2ac",
+    "url": "/testSocNetwork/index.html"
+  },
+  {
+    "revision": "6ad2698684e0a3194d84",
+    "url": "/testSocNetwork/static/css/3.f9e2a330.chunk.css"
+  },
+  {
+    "revision": "46414ec3dc1d03b4d511",
+    "url": "/testSocNetwork/static/css/4.856a2bc8.chunk.css"
+  },
+  {
+    "revision": "5fcd2d7e2112d78f63c4",
+    "url": "/testSocNetwork/static/css/main.5e05aeb1.chunk.css"
+  },
+  {
+    "revision": "0e5b398fea6fdda3ea17",
+    "url": "/testSocNetwork/static/js/2.24215a74.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/testSocNetwork/static/js/2.24215a74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad2698684e0a3194d84",
+    "url": "/testSocNetwork/static/js/3.804e9026.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/testSocNetwork/static/js/3.804e9026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46414ec3dc1d03b4d511",
+    "url": "/testSocNetwork/static/js/4.814373c2.chunk.js"
+  },
+  {
+    "revision": "5fcd2d7e2112d78f63c4",
+    "url": "/testSocNetwork/static/js/main.ec96ea18.chunk.js"
+  },
+  {
+    "revision": "9e14b8fc1cab4c3e76a2",
+    "url": "/testSocNetwork/static/js/runtime-main.393c5f67.js"
+  },
+  {
+    "revision": "68a5aa104457ecac4d4136285a830e3e",
+    "url": "/testSocNetwork/static/media/man-avatar-profile-round-icon_24640-14046.68a5aa10.jpg"
+  },
+  {
+    "revision": "abffbc113b851cfd4387f3a08766d75a",
+    "url": "/testSocNetwork/static/media/preloader.abffbc11.svg"
+  }
+]);
