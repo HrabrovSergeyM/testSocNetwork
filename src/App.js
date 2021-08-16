073ac01327516275d72cc303421de31cdd656755
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-16 17:02:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HeaderContainer from "./components/Header/HeaderContainer";
 import Navbar from "./components/Navbar/Navbar";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import {
-  BrowserRouter,
   HashRouter,
   Redirect,
   Route,
@@ -14,8 +13,6 @@ import {
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-// import DialogsContainer from "./components/Dialogs/DialogsContainer";
-// import UsersContainer from "./components/Users/UsersContainer";
 import Login from "./components/Login/Login";
 import { Component } from "react";
 import { connect } from "react-redux";
