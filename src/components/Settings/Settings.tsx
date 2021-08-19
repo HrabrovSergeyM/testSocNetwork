f8e0f62da656a7d467902960a46f688df5a508b1
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-20 02:26:00 +0300

    Create Settings.tsx

---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Settings.module.css'
+
+const Settings: React.FC = () => {
+    return (
+        <div>
+        <p>
+            Settings
+        </p>
+    </div>
+    )
+}
+
+export default Settings
