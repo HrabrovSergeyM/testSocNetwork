713e3ce1cb50fbc8ffdce92bfbd69e9abf435c41
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-20 02:25:24 +0300

    Create News.tsx

---
+++ src/components/News/News.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './News.module.css'
+
+const News: React.FC = () => {
+    return (
+    <div>
+        <p>
+            News
+        </p>
+    </div>
+    )
+}
+
+export default News
