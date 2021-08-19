877e8b120b5c6401676bd6f6253da415f1400486
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-20 02:26:19 +0300

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as serviceWorker from "./serviceWorker";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import MainApp from "./App";
+
+ReactDOM.render(<MainApp />, document.getElementById("root"));
+
+serviceWorker.unregister();
