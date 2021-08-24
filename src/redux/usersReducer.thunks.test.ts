4e6ce759f223d8ade2bc6a2632b9a9bdb8df3172
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-24 04:31:27 +0300

    Update usersReducer.thunks.test.ts

--- src/redux/usersReducer.thunks.test.ts
+++ src/redux/usersReducer.thunks.test.ts
@@ -1,24 +1,48 @@
 import { APIResponseType, ResultCodesEnum } from './../api/api';
 import { usersAPI } from './../api/usersApi';
-import { follow } from "./usersReducer"
+import { actions, follow, unfollow } from "./usersReducer"
 
 jest.mock('./../api/usersApi')
-const usersAPIMock = usersAPI
+
+const usersAPIMock = usersAPI as jest.Mocked<typeof usersAPI>
 const result: APIResponseType = {
     resultCode: ResultCodesEnum.Success,
     messages: [],
     data: {}
 }
 
-//@ts-ignore
+const dispatchMock = jest.fn()
+const getStateMock = jest.fn()
+
+beforeEach(() => {
+    dispatchMock.mockClear()
+    getStateMock.mockClear()
+    usersAPIMock.follow.mockClear()
+    usersAPIMock.unfollow.mockClear()
+})
+
 usersAPIMock.follow.mockReturnValue(Promise.resolve(result))
+usersAPIMock.unfollow.mockReturnValue(Promise.resolve(result))
 
-test('', async () => {
+
+test('success follow thunk', async () => {
     const thunk = follow(1)
-    const dispatchMock = jest.fn()
 
-    //@ts-ignore
-    await thunk(dispatchMock)
+    await thunk(dispatchMock, getStateMock, {})
 
     expect(dispatchMock).toBeCalledTimes(3)
+    expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.setFollowingProgress(true, 1))
+    expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess(1))
+    expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.setFollowingProgress(false, 1))
+})
+
+test('success unfollow thunk', async () => {
+    const thunk = unfollow(1)
+
+    await thunk(dispatchMock, getStateMock, {})
+
+    expect(dispatchMock).toBeCalledTimes(3)
+    expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.setFollowingProgress(true, 1))
+    expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.unfollowSuccess(1))
+    expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.setFollowingProgress(false, 1))
 })
