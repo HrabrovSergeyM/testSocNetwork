e9b43bac8427f27735a46c31f1bd89e22d2c37be
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-26 21:41:32 +0300

    Update Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -8,8 +8,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCurrentPage, getFollowingInProgress, getPageSize, getTotalUserCount, getUsers, getUsersFilter } from "../../redux/usersSelectors";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import * as queryString from 'querystring'
 
 type PropsType = {
+}
+type QueryParamsType = {
+  term?: string
+  page?: string
+  friend?: string
 }
 
 export const Users: React.FC<PropsType> = (props) => {
@@ -25,23 +31,45 @@ export const Users: React.FC<PropsType> = (props) => {
 
   const history = useHistory()
 
-  useEffect(()=>{
+  useEffect(() => {
+    const parsed = queryString.parse(history.location.search.substr(1)) as QueryParamsType
+
+    let actualPage = currentPage
+    let actualFilter = filter
+
+    if (!!parsed.page) actualPage = Number(parsed.page)
+
+
+    if (!!parsed.term) actualFilter = {...actualFilter, term: parsed.term as string}
+
+    switch(parsed.friend) {
+        case "null":
+            actualFilter = {...actualFilter, friend: null}
+            break;
+        case "true":
+            actualFilter = {...actualFilter, friend: true}
+            break;
+        case "false":
+            actualFilter = {...actualFilter, friend: false}
+            break;
+    }
+
+    dispatch(requestUsers(actualPage, pageSize, actualFilter))
+}, [])
+
+useEffect(() => {
+    const query: QueryParamsType = {}
+
+    if (!!filter.term) query.term = filter.term
+    if (filter.friend !== null) query.friend = String(filter.friend)
+    if (currentPage !== 1) query.page = String(currentPage)
+
     history.push({
-      pathname: `/users`,
-      search: `?term=${filter.term}&friend=${filter.friend}&page=${currentPage}`
-
+        pathname: '/users',
+        search: queryString.stringify(query)
     })
-  }, [filter, currentPage])
-  // useEffect(() => {
-   
-  //   const {search} = history.location
-  //   // const parsed = queryString.parse(search)
-  //   // const search = history.location.search
-  //   const params = new URLSearchParams(search);
-  //   const parsedTerm = params.get("term")
+}, [filter, currentPage])
 
-  //   dispatch(requestUsers(currentPage, pageSize, filter));
-  // },[])
 
   const follow = (userId: number) => {
     dispatch(follow(userId))
