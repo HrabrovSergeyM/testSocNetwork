531a628065dffe161e07d3ae2c4e066f359e82b2
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-26 16:23:20 +0300

    Update Users.tsx

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,23 +6,38 @@ import { UserType } from "../../types/types";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { submit } from "redux-form";
 import { UsersSearchForm } from "./UsersSearchForm";
-import { FilterType } from "../../redux/usersReducer";
+import { FilterType, requestUsers } from "../../redux/usersReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { getCurrentPage, getPageSize, getTotalUserCount, getUsers, getUsersFilter } from "../../redux/usersSelectors";
 
 type PropsType = {
-  totalUserCount: number,
-  pageSize: number,
-  currentPage: number,
   isFetching: boolean
-  users: Array<UserType>
   followingInProgress: Array<number>
-  onPageChanged: (pageNumber: number) => void,
-  onFilterChanged: (filter: FilterType) => void
   follow: (userId: number) => void
   unfollow: (userId: number) => void
 }
 
 let Users: React.FC<PropsType> = (props) => {
-  let pagesCount = Math.ceil(props.totalUserCount / props.pageSize);
+
+  const users = useSelector(getUsers) 
+  const totalUserCount = useSelector(getTotalUserCount) 
+  const currentPage = useSelector(getCurrentPage)
+  const pageSize = useSelector(getPageSize)
+  const filter = useSelector(getUsersFilter)
+
+  const dispatch = useDispatch()
+
+
+
+  const onPageChanged = (pageNumber: number) => {
+    dispatch(requestUsers(pageNumber, pageSize, filter));
+  }
+  const onFilterChanged = (filter: FilterType) => {
+    dispatch(requestUsers(1, pageSize, filter));
+    
+  }
+
+  let pagesCount = Math.ceil(totalUserCount / pageSize);
   let pages = [];
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
@@ -30,16 +45,16 @@ let Users: React.FC<PropsType> = (props) => {
 
   return (
     <div>
-      <UsersSearchForm onFilterChanged={props.onFilterChanged}/>
+      <UsersSearchForm onFilterChanged={onFilterChanged}/>
       <Paginator
-        currentPage={props.currentPage}
-        onPageChanged={props.onPageChanged}
+        currentPage={currentPage}
+        onPageChanged={onPageChanged}
         
-        totalUserCount={props.totalUserCount}
-        pageSize={props.pageSize}
+        totalUserCount={totalUserCount}
+        pageSize={pageSize}
       />
       <div>
-        {props.users.map((user) => (
+        {users.map((user) => (
           <User
             user={user}
             followingInProgress={props.followingInProgress}
