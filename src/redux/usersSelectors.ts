c53671197fa30c02f2a92cfe3e8f2281e7ff2be5
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-26 18:04:09 +0300

    Update usersSelectors.ts

--- src/redux/usersSelectors.ts
+++ src/redux/usersSelectors.ts
@@ -1,13 +1,15 @@
+import { createSelector } from 'reselect';
 import { AppStateType } from './reduxStore';
-import { createSelector } from "reselect";
 
-const getAllUsers = (state: AppStateType) => {
+
+const getUsersSelector = (state: AppStateType) => {
   return state.usersPage.users;
-};
+}
 
-export const getUsersSelector = createSelector(getAllUsers, (users) => {
-  return users.filter((u) => true);
-});
+export const getUsers = createSelector(getUsersSelector,
+  (users) => {
+  return users.filter(u => true);
+})
 
 export const getPageSize = (state: AppStateType) => {
   return state.usersPage.pageSize;
