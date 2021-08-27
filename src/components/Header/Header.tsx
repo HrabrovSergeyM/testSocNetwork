7f8febabb55824c2854c7102bfa8d0a8668f7c6f
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 10:52:41 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,55 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import 'antd/dist/antd.css';
-import { Layout, Menu, Avatar, Row, Col, Button } from 'antd';
-import { UserOutlined} from '@ant-design/icons';
+import "antd/dist/antd.css";
+import { Layout, Menu, Avatar, Row, Col, Button } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
-import { selectCurrentUserLogin, selectIsAuth } from "../../redux/authSelectors";
+import {
+  selectCurrentUserLogin,
+  selectIsAuth,
+} from "../../redux/authSelectors";
 import { logout } from "../../redux/authReducer";
 
-
-export type MapPropsType = {
-}
-
-
+export type MapPropsType = {};
 
 export const Header: React.FC<MapPropsType> = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const login = useSelector(selectCurrentUserLogin);
 
-  const isAuth = useSelector(selectIsAuth)
-  const login = useSelector(selectCurrentUserLogin)
-
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const logoutCallback = () => {
-    dispatch(logout())
-  }
+    dispatch(logout());
+  };
 
-  const {Header} = Layout
+  const { Header } = Layout;
 
-  return <Header className="header">
-    <Row>
+  return (
+    <Header className="header">
+      <Row>
         <Col span={18}>
-            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                <Menu.Item key="1"><Link to="/users">Users</Link></Menu.Item>
-            </Menu>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+            <Menu.Item key="1">
+              <Link to="/users">Users</Link>
+            </Menu.Item>
+          </Menu>
         </Col>
 
-        {isAuth
-            ?<> <Col span={1}>
-                <Avatar alt={login || ''} style={{backgroundColor: '#87d068'}} icon={<UserOutlined/>}/>
+        {isAuth ? (
+          <>
+            {" "}
+            <Col span={1}>
+              <Avatar
+                alt={login || ""}
+                style={{ backgroundColor: "#87d068" }}
+                icon={<UserOutlined />}
+              />
             </Col>
-                <Col span={5}>
-                    <Button onClick={logoutCallback}>Log out</Button>
-                </Col>
-            </>
-            : <Col span={6}>
-                <Button>
-                    <Link to={'/login'}>Login</Link>
-                </Button>
-            </Col>}
-
-    </Row>
-  </Header>
-  }
-
-
+            <Col span={5}>
+              <Button onClick={logoutCallback}>Log out</Button>
+            </Col>
+          </>
+        ) : (
+          <Col span={6}>
+            <Button>
+              <Link to={"/login"}>Login</Link>
+            </Button>
+          </Col>
+        )}
+      </Row>
+    </Header>
+  );
+};
