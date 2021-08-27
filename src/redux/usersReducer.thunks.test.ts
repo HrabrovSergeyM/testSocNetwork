37a12a5382ca178f28c020dbfe767db3a8af0ec2
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 10:56:35 +0300

    Update usersReducer.thunks.test.ts

--- src/redux/usersReducer.thunks.test.ts
+++ src/redux/usersReducer.thunks.test.ts
@@ -1,48 +1,59 @@
-import { APIResponseType, ResultCodesEnum } from './../api/api';
-import { usersAPI } from './../api/usersApi';
-import { actions, follow, unfollow } from "./usersReducer"
+import { APIResponseType, ResultCodesEnum } from "./../api/api";
+import { usersAPI } from "./../api/usersApi";
+import { actions, follow, unfollow } from "./usersReducer";
 
-jest.mock('./../api/usersApi')
+jest.mock("./../api/usersApi");
 
-const usersAPIMock = usersAPI as jest.Mocked<typeof usersAPI>
+const usersAPIMock = usersAPI as jest.Mocked<typeof usersAPI>;
 const result: APIResponseType = {
-    resultCode: ResultCodesEnum.Success,
-    messages: [],
-    data: {}
-}
+  resultCode: ResultCodesEnum.Success,
+  messages: [],
+  data: {},
+};
 
-const dispatchMock = jest.fn()
-const getStateMock = jest.fn()
+const dispatchMock = jest.fn();
+const getStateMock = jest.fn();
 
 beforeEach(() => {
-    dispatchMock.mockClear()
-    getStateMock.mockClear()
-    usersAPIMock.follow.mockClear()
-    usersAPIMock.unfollow.mockClear()
-})
+  dispatchMock.mockClear();
+  getStateMock.mockClear();
+  usersAPIMock.follow.mockClear();
+  usersAPIMock.unfollow.mockClear();
+});
 
-usersAPIMock.follow.mockReturnValue(Promise.resolve(result))
-usersAPIMock.unfollow.mockReturnValue(Promise.resolve(result))
+usersAPIMock.follow.mockReturnValue(Promise.resolve(result));
+usersAPIMock.unfollow.mockReturnValue(Promise.resolve(result));
 
+test("success follow thunk", async () => {
+  const thunk = follow(1);
 
-test('success follow thunk', async () => {
-    const thunk = follow(1)
+  await thunk(dispatchMock, getStateMock, {});
 
-    await thunk(dispatchMock, getStateMock, {})
+  expect(dispatchMock).toBeCalledTimes(3);
+  expect(dispatchMock).toHaveBeenNthCalledWith(
+    1,
+    actions.setFollowingProgress(true, 1)
+  );
+  expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess(1));
+  expect(dispatchMock).toHaveBeenNthCalledWith(
+    3,
+    actions.setFollowingProgress(false, 1)
+  );
+});
 
-    expect(dispatchMock).toBeCalledTimes(3)
-    expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.setFollowingProgress(true, 1))
-    expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.followSuccess(1))
-    expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.setFollowingProgress(false, 1))
-})
+test("success unfollow thunk", async () => {
+  const thunk = unfollow(1);
 
-test('success unfollow thunk', async () => {
-    const thunk = unfollow(1)
+  await thunk(dispatchMock, getStateMock, {});
 
-    await thunk(dispatchMock, getStateMock, {})
-
-    expect(dispatchMock).toBeCalledTimes(3)
-    expect(dispatchMock).toHaveBeenNthCalledWith(1, actions.setFollowingProgress(true, 1))
-    expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.unfollowSuccess(1))
-    expect(dispatchMock).toHaveBeenNthCalledWith(3, actions.setFollowingProgress(false, 1))
-})
+  expect(dispatchMock).toBeCalledTimes(3);
+  expect(dispatchMock).toHaveBeenNthCalledWith(
+    1,
+    actions.setFollowingProgress(true, 1)
+  );
+  expect(dispatchMock).toHaveBeenNthCalledWith(2, actions.unfollowSuccess(1));
+  expect(dispatchMock).toHaveBeenNthCalledWith(
+    3,
+    actions.setFollowingProgress(false, 1)
+  );
+});
