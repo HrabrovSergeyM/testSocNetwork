dc146fbe532a10d4ce2932d3fde78257fc6e03b3
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 10:52:31 +0300

    Update DialogsContainer.tsx

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -10,12 +10,11 @@ import { AppStateType } from "../../redux/reduxStore";
 
 let mapStateToProps = (state: AppStateType) => {
   return {
-    dialogsPage: state.messagesPage
+    dialogsPage: state.messagesPage,
   };
 };
 
-
 export default compose<React.ComponentType>(
-  connect(mapStateToProps, {sendMessage: actions.sendMessage}),
+  connect(mapStateToProps, { sendMessage: actions.sendMessage }),
   withAuthRedirect
 )(Dialogs);
