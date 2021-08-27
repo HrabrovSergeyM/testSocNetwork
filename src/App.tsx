6622f8c44386715c0ac271f62db0f131792cd737
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 10:12:24 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import Navbar from "./components/Navbar/Navbar";
+import 'antd/dist/antd.css';
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import {
   HashRouter,
+  Link,
   Redirect,
   Route,
   Switch,
@@ -23,7 +23,10 @@ import store, { AppStateType } from "./redux/reduxStore";
 import { Provider } from "react-redux";
 import { Suspense } from "react";
 import { withSuspense } from "./hoc/withSuspense";
-// import { UsersPage } from "./components/Users/UsersContainer";
+import { Layout, Menu, Breadcrumb} from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { Header } from "./components/Header/Header";
+
 
 const DialogsContainer = React.lazy(() =>
   import("./components/Dialogs/DialogsContainer")
@@ -62,49 +65,129 @@ class App extends Component<MapsPropsType & DispatchPropsType> {
     if (!this.props.initialized) {
       return <Preloader />;
     }
+    const { SubMenu } = Menu;
+    const { Content, Footer, Sider } = Layout;
 
     return (
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar />
-        <Switch>
-          <div className="app-wrapper-content">
-            <Route exact path="/" render={() => <Redirect to={"/profile"} />} />
+      // <div className="app-wrapper">
+      //   <HeaderContainer />
+      //   <Navbar />
+      //   <Switch>
+      //     <div className="app-wrapper-content">
+      //       <Route exact path="/" render={() => <Redirect to={"/profile"} />} />
 
-            <Route
-              path="/profile/:userId?"
-              render={() => <SuspendedProfile />}
-            />
-            <Route
-              exact
-              path="/dialogs"
-              render={() => <SuspendedDialogs/>}
-            />
-            <Route
-              path="/users"
-              render={() => {
-                return (
-                  <Suspense
-                    fallback={
-                      <div>
-                        <Preloader />
-                      </div>
-                    }
+      //       <Route
+      //         path="/profile/:userId?"
+      //         render={() => <SuspendedProfile />}
+      //       />
+      //       <Route
+      //         exact
+      //         path="/dialogs"
+      //         render={() => <SuspendedDialogs/>}
+      //       />
+      //       <Route
+      //         path="/users"
+      //         render={() => {
+      //           return (
+      //             <Suspense
+      //               fallback={
+      //                 <div>
+      //                   <Preloader />
+      //                 </div>
+      //               }
+      //             >
+      //               <UsersPage
+      //                 pageTitle={"Guys who want to learn React too"}
+      //               />
+      //             </Suspense>
+      //           );
+      //         }}
+      //       />
+      //       <Route path="/news" render={() => <News />} />
+      //       <Route path="/music" render={() => <Music />} />
+      //       <Route path="/settings" render={() => <Settings />} />
+      //       <Route path="/login" render={() => <LoginPage />} />
+      //     </div>
+      //   </Switch>
+      // </div>
+      <Layout>
+     <Header/>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%' }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="My Profile">
+                <Menu.Item key="1"><Link to="/profile" >
+          Profile
+        </Link></Menu.Item>
+                <Menu.Item key="2"><Link to="/dialogs" >
+          Dialogs
+        </Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub2" icon={<LaptopOutlined />} title="Users">
+                <Menu.Item key="3"><Link to="/users" >
+          Users
+        </Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key="sub3" icon={<NotificationOutlined />} title="Others">
+                <Menu.Item key="4"><Link to="/news">News</Link></Menu.Item>
+                <Menu.Item key="5"><Link to="/music">Music</Link></Menu.Item>
+                <Menu.Item key="6"><Link to="/settings">Settings</Link></Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Content style={{ padding: '0 24px', minHeight: 280 }}>
+             <Switch>
+              <div className="app-wrapper-content">
+                <Route 
+                    exact 
+                    path="/" 
+                    render={() => <Redirect to={"/profile"} />} />
+                <Route
+                    path="/profile/:userId?"
+                    render={() => <SuspendedProfile />}
+                  />
+                <Route
+                    exact
+                    path="/dialogs"
+                    render={() => <SuspendedDialogs/>}
+                />
+                <Route
+                    path="/users"
+                    render={() => {
+                      return (
+                        <Suspense
+                          fallback={
+                            <div>
+                              <Preloader />
+                            </div>
+                          }
                   >
-                    <UsersPage
-                      pageTitle={"Guys who want to learn React too"}
-                    />
-                  </Suspense>
-                );
-              }}
-            />
-            <Route path="/news" render={() => <News />} />
-            <Route path="/music" render={() => <Music />} />
-            <Route path="/settings" render={() => <Settings />} />
-            <Route path="/login" render={() => <LoginPage />} />
-          </div>
-        </Switch>
-      </div>
+                          <UsersPage
+                            pageTitle={"Guys who want to learn React too"}
+                          />
+                        </Suspense>
+                      );
+                    }}
+                />
+                <Route path="/news" render={()=> <News />} />
+                <Route path="/music" render={() => <Music />} />
+                <Route path="/settings" render={() => <Settings />} />
+                <Route path="/login" render={() => <LoginPage />} />
+              </div>
+            </Switch>
+          </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Social Network ©2021 Created by Sergey Hrabrov</Footer>
+    </Layout>
     );
   }
 }
