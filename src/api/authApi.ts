efdedf7cfde236d2ae80824d22b437de43657bda
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 10:51:46 +0300

    Update authApi.ts

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,28 +1,40 @@
 import { instance, APIResponseType, ResultCodesEnum } from "./api";
 
-type MeResponseDataType = { 
-      id: number
-      email: string
-      login: string
-  }
-  
+type MeResponseDataType = {
+  id: number;
+  email: string;
+  login: string;
+};
+
 type LoginResponseDataType = {
-      userId: number
-  }
+  userId: number;
+};
 
 export const authAPI = {
-    me() {
-      return instance.get<APIResponseType<MeResponseDataType>>(`auth/me`).then(res => res.data);
-    },
-    login(email: string, password: string, rememberMe = false, captcha: null | string = null) {
-      return instance.post<APIResponseType<LoginResponseDataType, ResultCodesEnum>>("auth/login", {
-        email,
-        password,
-        rememberMe,
-        captcha,
-      }).then(res => res.data);
-    },
-    logout() {
-      return instance.delete("auth/login");
-    },
-  };
+  me() {
+    return instance
+      .get<APIResponseType<MeResponseDataType>>(`auth/me`)
+      .then((res) => res.data);
+  },
+  login(
+    email: string,
+    password: string,
+    rememberMe = false,
+    captcha: null | string = null
+  ) {
+    return instance
+      .post<APIResponseType<LoginResponseDataType, ResultCodesEnum>>(
+        "auth/login",
+        {
+          email,
+          password,
+          rememberMe,
+          captcha,
+        }
+      )
+      .then((res) => res.data);
+  },
+  logout() {
+    return instance.delete("auth/login");
+  },
+};
