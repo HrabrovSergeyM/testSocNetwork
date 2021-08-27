8b6c9c99fea3ba6081038d8185839ef1f4ef6f0c
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 10:52:27 +0300

    Update Dialogs.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import classes from './Dialogs.module.css';
+import React from "react";
+import classes from "./Dialogs.module.css";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import AddMessageForm from "./AddMessageForm/AddMessageForm";
-import {InitialStateType} from '../../redux/dialogsReducer';
+import { InitialStateType } from "../../redux/dialogsReducer";
 
 type PropsType = {
-    dialogsPage: InitialStateType
-    sendMessage: (messageText: string) => void
-}
+  dialogsPage: InitialStateType;
+  sendMessage: (messageText: string) => void;
+};
 
 export type NewMessageFormValuesType = {
-    newMessageBody: string
-}
+  newMessageBody: string;
+};
 
 const Dialogs: React.FC<PropsType> = (props) => {
-    let state = props.dialogsPage
+  let state = props.dialogsPage;
 
-    let dialogsElements = state.dialogsData.map( d => <DialogItem name={d.name} key={d.id} id={d.id} />  );
-    let messagesElements = state.messagesData.map( m => <Message message={m.message} key={m.id} /> );
+  let dialogsElements = state.dialogsData.map((d) => (
+    <DialogItem name={d.name} key={d.id} id={d.id} />
+  ));
+  let messagesElements = state.messagesData.map((m) => (
+    <Message message={m.message} key={m.id} />
+  ));
 
-    let addNewMessage = (values: NewMessageFormValuesType) => {
-        props.sendMessage(values.newMessageBody);
-    }
+  let addNewMessage = (values: NewMessageFormValuesType) => {
+    props.sendMessage(values.newMessageBody);
+  };
 
-    return (
-        <div className={classes.dialogs}>
-            <div className={classes.dialogsItems}>
-                { dialogsElements }
-            </div>
-            <div className={classes.messages}>
-                <div>{ messagesElements }</div>
-            </div>
-            <AddMessageForm onSubmit={addNewMessage} />
-        </div>
-    )
-}
+  return (
+    <div className={classes.dialogs}>
+      <div className={classes.dialogsItems}>{dialogsElements}</div>
+      <div className={classes.messages}>
+        <div>{messagesElements}</div>
+      </div>
+      <AddMessageForm onSubmit={addNewMessage} />
+    </div>
+  );
+};
 
 export default Dialogs;
