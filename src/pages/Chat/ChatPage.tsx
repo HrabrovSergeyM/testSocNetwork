7f6ace3d013f9a377940d993b2d7a2977122d560
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 11:03:48 +0300

    Update ChatPage.tsx

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const ChatPage: React.FC = () => {
-  return <div>Chat</div>;
+  return (
+    <div>
+      <Chat />
+    </div>
+  );
+};
+
+const Chat: React.FC = () => {
+  return (
+    <div>
+      <Messages />
+      <AddMessageFormChat />
+    </div>
+  );
+};
+
+const Messages: React.FC = () => {
+  return <div>Messages</div>;
+};
+
+const AddMessageFormChat: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <textarea></textarea>
+      </div>
+      <div>
+        <button>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
