48e5b44a45b8ed151d494894ce1ca9b90b7eeed2
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 10:56:30 +0300

    Update usersReducer.test.ts

--- src/redux/usersReducer.test.ts
+++ src/redux/usersReducer.test.ts
@@ -1,41 +1,56 @@
-import { actions } from './usersReducer';
-import usersReducer, { InitialState } from './usersReducer';
+import { actions } from "./usersReducer";
+import usersReducer, { InitialState } from "./usersReducer";
 
 let state: InitialState;
 
 beforeEach(() => {
-    state = {
-        users: [
-            {
-                id: 0, name: 'Serg 0', followed: false, photos: {small: null, large: null}, status: 'status 0'
-            },
-            {
-                id: 1, name: 'Serg 1', followed: false, photos: {small: null, large: null}, status: 'status 1'
-            },
-            {
-                id: 2, name: 'Serg 2', followed: true, photos: {small: null, large: null}, status: 'status 2'
-            },
-            {
-                id: 3, name: 'Serg 3', followed: true, photos: {small: null, large: null}, status: 'status 3'
-            },],
-        pageSize: 10,
-        totalUserCount: 0,
-        currentPage: 1,
-        isFetching: false,
-        followingInProgress: [] 
-    }
-})
+  state = {
+    users: [
+      {
+        id: 0,
+        name: "Serg 0",
+        followed: false,
+        photos: { small: null, large: null },
+        status: "status 0",
+      },
+      {
+        id: 1,
+        name: "Serg 1",
+        followed: false,
+        photos: { small: null, large: null },
+        status: "status 1",
+      },
+      {
+        id: 2,
+        name: "Serg 2",
+        followed: true,
+        photos: { small: null, large: null },
+        status: "status 2",
+      },
+      {
+        id: 3,
+        name: "Serg 3",
+        followed: true,
+        photos: { small: null, large: null },
+        status: "status 3",
+      },
+    ],
+    pageSize: 10,
+    totalUserCount: 0,
+    currentPage: 1,
+    isFetching: false,
+    followingInProgress: [],
+  };
+});
 
 test("follow success", () => {
- 
-    const newState = usersReducer(state, actions.followSuccess(1))
-    expect(newState.users[0].followed).toBeFalsy()
-    expect(newState.users[1].followed).toBeTruthy()
-}) 
+  const newState = usersReducer(state, actions.followSuccess(1));
+  expect(newState.users[0].followed).toBeFalsy();
+  expect(newState.users[1].followed).toBeTruthy();
+});
 
 test("unfollow success", () => {
- 
-    const newState = usersReducer(state, actions.unfollowSuccess(2))
-    expect(newState.users[0].followed).toBeFalsy()
-    expect(newState.users[2].followed).toBeFalsy()
-}) 
+  const newState = usersReducer(state, actions.unfollowSuccess(2));
+  expect(newState.users[0].followed).toBeFalsy();
+  expect(newState.users[2].followed).toBeFalsy();
+});
