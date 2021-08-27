8117846a9f051757a6a065757fc515b18b0bc6f4
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 12:38:00 +0300

    Update ChatPage.tsx

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,4 +1,17 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export type ChatMessageType = {
+  message: string;
+  photo: string;
+  userId: number;
+  userName: string;
+};
+
+const wsChannel = new WebSocket(
+  `wss://social-network.samuraijs.com/handlers/ChatHandler.ashx`
+);
 
 const ChatPage: React.FC = () => {
   return (
@@ -18,17 +31,54 @@ const Chat: React.FC = () => {
 };
 
 const Messages: React.FC = () => {
-  return <div>Messages</div>;
+  const [messages, setMessages] = useState<ChatMessageType[]>([]);
+  useEffect(() => {
+    wsChannel.addEventListener("message", (e: MessageEvent) => {
+      let newMessages = JSON.parse(e.data);
+      setMessages((prevMessages) => [...prevMessages, ...newMessages]);
+    });
+  }, []);
+  return (
+    <div style={{ height: "400px", overflowY: "auto" }}>
+      {messages.map((m, index) => (
+        <Message key={index} message={m} />
+      ))}
+    </div>
+  );
+};
+
+const Message: React.FC<{ message: ChatMessageType }> = ({ message }) => {
+  return (
+    <div>
+      <img src={message.photo} style={{ width: "30px" }} />{" "}
+      <b>{message.userName}</b>
+      <br />
+      {message.message}
+      <hr />
+    </div>
+  );
 };
 
 const AddMessageFormChat: React.FC = () => {
+  const [message, setMessage] = useState("");
+  const sendMessage = () => {
+    if (!message) {
+      return;
+    }
+    wsChannel.send(message);
+    setMessage("");
+  };
+
   return (
     <div>
       <div>
-        <textarea></textarea>
+        <textarea
+          onChange={(e) => setMessage(e.currentTarget.value)}
+          value={message}
+        ></textarea>
       </div>
       <div>
-        <button>Send</button>
+        <button onClick={sendMessage}>Send</button>
       </div>
     </div>
   );
