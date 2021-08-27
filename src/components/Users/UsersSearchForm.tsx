9eb3b318731cc917d6251447e794fe5eef507d5e
HrabrovSergeyM <hrabrov.s.m@gmail.com>
2021-08-27 10:55:42 +0300

    Update UsersSearchForm.tsx

--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -5,52 +5,64 @@ import { FilterType } from "../../redux/usersReducer";
 import { getUsersFilter } from "../../redux/usersSelectors";
 
 type PropsType = {
-    onFilterChanged: (filter: FilterType) => void
-}
-type FriendFormType = 'true' | 'false' | 'null'
+  onFilterChanged: (filter: FilterType) => void;
+};
+type FriendFormType = "true" | "false" | "null";
 type FormType = {
-    term: string
-    friend: FriendFormType
-}
+  term: string;
+  friend: FriendFormType;
+};
 
 const usersSearchFormValidate = (values: any) => {
-    const errors = {};
-    return errors;
-  }
-  
+  const errors = {};
+  return errors;
+};
+
 export const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
-    
-  const filter = useSelector(getUsersFilter)
-  
-  const submit = (values: FormType, { setSubmitting}: {setSubmitting: (isSubmitting: boolean) => void}) => {
-        const filter: FilterType = {
-            term: values.term,
-            friend: values.friend === 'null' ? null : values.friend === 'true' ? true : false
-        }
-        props.onFilterChanged(filter)
-        setSubmitting(false)
-    } 
-  
-    return <div>
+  const filter = useSelector(getUsersFilter);
+
+  const submit = (
+    values: FormType,
+    { setSubmitting }: { setSubmitting: (isSubmitting: boolean) => void }
+  ) => {
+    const filter: FilterType = {
+      term: values.term,
+      friend:
+        values.friend === "null"
+          ? null
+          : values.friend === "true"
+          ? true
+          : false,
+    };
+    props.onFilterChanged(filter);
+    setSubmitting(false);
+  };
+
+  return (
+    <div>
       <Formik
-          enableReinitialize
-          initialValues={{ term: filter.term, friend: String(filter.friend) as FriendFormType}}
-          validate={usersSearchFormValidate}
-          onSubmit={submit}
-       >
-         {({ isSubmitting }) => (
-           <Form>
-             <Field type="text" name="term" />
-             <Field name='friend' as='select'>
-                 <option value='null'>All</option>
-                 <option value='true'>Only Followed</option>
-                 <option value='false'>Only Unfollowed</option>
-             </Field>
-             <button type="submit" disabled={isSubmitting}>
-               Find
-             </button>
-           </Form>
-         )}
+        enableReinitialize
+        initialValues={{
+          term: filter.term,
+          friend: String(filter.friend) as FriendFormType,
+        }}
+        validate={usersSearchFormValidate}
+        onSubmit={submit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="term" />
+            <Field name="friend" as="select">
+              <option value="null">All</option>
+              <option value="true">Only Followed</option>
+              <option value="false">Only Unfollowed</option>
+            </Field>
+            <button type="submit" disabled={isSubmitting}>
+              Find
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
-})
+  );
+});
